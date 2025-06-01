7866de5aab66b8d23fe4f9baef52ca09d76cc22e
achariar <107132694+achariar@users.noreply.github.com>
2025-06-01 11:41:56 +0000

    Deploying to gh-pages from @ achariar/achariar.github.io@a9b8197d0902f0c30e3e5b685daa8a505339a739 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,19 +30,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-resources",
-          title: "resources",
-          description: "resources by categories in reversed chronological order.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
+          },
+        },{id: "nav-resources",
+          title: "resources",
+          description: "resources by categories in reversed chronological order.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
           },
         },{id: "nav-vitae",
           title: "vitae",
